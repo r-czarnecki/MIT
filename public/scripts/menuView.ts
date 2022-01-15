5474b7a68adb5d572c3d81982b981dce0f59cbb8
Marcin Kurowski <marcin@lofty.studio>
2022-01-15 18:59:45 +0100

    Display available floors

--- public/scripts/menuView.ts
+++ public/scripts/menuView.ts
@@ -1,16 +1,49 @@
+import { floors } from "./config.js";
+
 interface Window {
   test: any;
 }
 
-const pointersMenuExpanded = document.getElementById("pointersMenuExpanded");
-const pointersMenuCollapsed = document.getElementById("pointersMenuCollapsed");
+// INIT
 
-const pointersMenuOpen = () => {
-  pointersMenuCollapsed.style.display = "none";
-  pointersMenuExpanded.style.display = "block";
-};
+(() => {})();
 
-const pointersMenuClose = () => {
-  pointersMenuCollapsed.style.display = "flex";
-  pointersMenuExpanded.style.display = "none";
-};
+// FLOOR OPTIONS
+
+(() => {
+  const floorsOptions = document.getElementById("floorsOptions");
+
+  const floorsMap = new Map(Object.entries(floors));
+  floorsMap.forEach((_, floor) => {
+    let FloorButton = document.createElement("button");
+    FloorButton.innerHTML = floor;
+    FloorButton.classList.add("floorButton");
+    FloorButton.addEventListener("click", () => {
+      console.log("floor", floor);
+    });
+    floorsOptions.appendChild(FloorButton);
+  });
+})();
+
+// POINTERS MENU
+
+(() => {
+  const pointersMenuExpanded = document.getElementById("pointersMenuExpanded");
+  const pointersMenuCollapsed = document.getElementById(
+    "pointersMenuCollapsed"
+  );
+  const closePointersMenu = document.getElementById("closePointersMenu");
+
+  const pointersMenuOpen = () => {
+    pointersMenuCollapsed.style.display = "none";
+    pointersMenuExpanded.style.display = "block";
+  };
+
+  const pointersMenuClose = () => {
+    pointersMenuCollapsed.style.display = "flex";
+    pointersMenuExpanded.style.display = "none";
+  };
+
+  closePointersMenu.addEventListener("click", pointersMenuClose);
+  pointersMenuCollapsed.addEventListener("click", pointersMenuOpen);
+})();
