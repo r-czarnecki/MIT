c17d7251f5af244a1d8daff423760db91f15b534
Marcin Kurowski <marcin@lofty.studio>
2022-01-15 14:21:45 +0100

    Rename views

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
-import express from 'express'
-import csurf from 'csurf';
-import session from 'express-session';
-import connectSqlite from 'connect-sqlite3';
-import cookieParser from 'cookie-parser';
-import { introRouter } from './routes/introRouter.js';
-import { mapRouter } from './routes/mapRouter.js';
-import { mapViewRouter } from './routes/mapViewRouter.js';
+import express from "express";
+import csurf from "csurf";
+import session from "express-session";
+import connectSqlite from "connect-sqlite3";
+import cookieParser from "cookie-parser";
+import { introRouter } from "./routes/introRouter.js";
+import { menuRouter } from "./routes/menuRouter.js";
+import { mapRouter } from "./routes/mapRouter.js";
 
 const app = express();
 
@@ -16,22 +16,26 @@ const csrfProtection = csurf({ cookie: true });
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
-app.use(session({
+app.use(
+  session({
     resave: false,
     saveUninitialized: true,
-    secret: 'sdfsdfsdjfhiu',
-    store: new SQLiteStore()
-}));
-app.use(express.static('public'));
+    secret: "sdfsdfsdjfhiu",
+    store: new SQLiteStore(),
+  })
+);
+app.use(express.static("public"));
 app.use(csrfProtection);
 
-app.set('view engine', 'pug');
+app.set("view engine", "pug");
 
-app.use('/', introRouter);
-app.use('/map', mapRouter);
-app.use('/mapView', mapViewRouter);
+app.use("/", introRouter);
+app.use("/menu", menuRouter);
+app.use("/map", mapRouter);
 
 app.listen(3000, () => {
-    console.log(`App is running at http://localhost:3000/ in ${app.get('env')} mode`);
-    console.log('Press Ctrl+C to stop.');
+  console.log(
+    `App is running at http://localhost:3000/ in ${app.get("env")} mode`
+  );
+  console.log("Press Ctrl+C to stop.");
 });
