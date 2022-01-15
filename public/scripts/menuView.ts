217c8707942bbadd3e0e75f083bed52e7327bbe1
Marcin Kurowski <marcin_kurowski@yahoo.pl>
2022-01-15 19:25:48 +0100

    Choose floor (#6)
    * Display available floors
    
    * Allow to choose floor
    
    * Init and remember the floor
    
    Co-authored-by: Marcin Kurowski <marcin@lofty.studio>

--- public/scripts/menuView.ts
+++ public/scripts/menuView.ts
@@ -1,16 +1,70 @@
+import { floors } from "./config.js";
+
 interface Window {
   test: any;
 }
+(() => {
+  const mapIFrame = document.querySelector("iframe").contentWindow as any;
 
-const pointersMenuExpanded = document.getElementById("pointersMenuExpanded");
-const pointersMenuCollapsed = document.getElementById("pointersMenuCollapsed");
+  // STATE
+  let state: {
+    currentFloor: number;
+  };
 
-const pointersMenuOpen = () => {
-  pointersMenuCollapsed.style.display = "none";
-  pointersMenuExpanded.style.display = "block";
-};
+  // INIT
 
-const pointersMenuClose = () => {
-  pointersMenuCollapsed.style.display = "flex";
-  pointersMenuExpanded.style.display = "none";
-};
+  mapIFrame.onload = () => {
+    state = {
+      currentFloor: 1,
+    };
+    showFloor(state.currentFloor);
+  };
+
+  // FLOOR OPTIONS
+
+  const showFloor = (floor: number) => {
+    mapIFrame.showFloor(floor);
+    document
+      .getElementById(`floorButton-${state.currentFloor}`)
+      .classList.remove("floorButton__active");
+    document
+      .getElementById(`floorButton-${floor}`)
+      .classList.add("floorButton__active");
+    state.currentFloor = floor;
+  };
+
+  const floorsOptions = document.getElementById("floorsOptions");
+
+  const floorsMap = new Map(Object.entries(floors));
+  floorsMap.forEach((_, floor) => {
+    let FloorButton = document.createElement("button");
+    FloorButton.innerHTML = floor;
+    FloorButton.classList.add("floorButton");
+    FloorButton.id = `floorButton-${floor}`;
+    FloorButton.addEventListener("click", () => {
+      showFloor(parseInt(floor));
+    });
+    floorsOptions.appendChild(FloorButton);
+  });
+
+  // POINTERS MENU
+
+  const pointersMenuExpanded = document.getElementById("pointersMenuExpanded");
+  const pointersMenuCollapsed = document.getElementById(
+    "pointersMenuCollapsed"
+  );
+  const closePointersMenu = document.getElementById("closePointersMenu");
+
+  const pointersMenuOpen = () => {
+    pointersMenuCollapsed.style.display = "none";
+    pointersMenuExpanded.style.display = "block";
+  };
+
+  const pointersMenuClose = () => {
+    pointersMenuCollapsed.style.display = "flex";
+    pointersMenuExpanded.style.display = "none";
+  };
+
+  closePointersMenu.addEventListener("click", pointersMenuClose);
+  pointersMenuCollapsed.addEventListener("click", pointersMenuOpen);
+})();
